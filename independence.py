17eaef6fcb83e13b3f33ef84a7fafd7c7684faf5
davanderlinden <46512605+davanderlinden@users.noreply.github.com>
2019-04-23 21:41:50 -0500

    Update independence.py

--- independence.py
+++ independence.py
@@ -1,25 +1,46 @@
+# -*- coding: utf-8 -*-
+
 import networkx as nx
 from itertools import combinations
 from functions.global_properties import V
 from functions.local_properties import neighbors
 
-G = nx.read_edgelist('graph_library\pan.txt')
-
-def is_independent(G,s):
-    for v in s:
-        if list(set(s) & set(neighbors(G,v))) != []:
-            return False
+def is_independent(G,S):
+    for s in S:
+        A = list(neighbors(G, s))
+        for a in A:
+            if a in S:
+                return False
     return True
-
-def maximum_independent_set(G):
+            
+        
+def max_independent_set(G):
     n = len(V(G))
     for k in range(n-1,1,-1):
-        for s in combinations(V(G),k):
-            if is_independent(G,list(s)) == True:
-                return list(s)
+        for S in combinations(V(G),k):
+            if is_independent(G,list(S)) == True:
+                return list(S)
             
 def independence_number(G):
-    return len(maximum_independent_set(G))
+    return len(max_independent_set(G))
 
 
 
+
+def is_independent_spes(G,S,v):
+    for s in S:
+        A = list(neighbors(G, s))
+        for a in A:
+            if a in S:
+                return False
+            if v not in S:
+                return False
+            
+    return True
+
+def independent_set(G,v):
+    n = len(V(G))
+    for k in range(n-1,1,-1):
+        for S in combinations(V(G),k):
+            if is_independent_spes(G,list(S),v) == True:
+                return list(S)
